9cc10eb1f42bd3a5a9a241963b613d79b581ff9c
vikto <vikto@FX517ZC>
2024-10-14 18:24:16 +0300

    Add project files.

---
+++ MyConverter/Converter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConverter
+{
+    internal class Converter
+    {
+        public const decimal DollarRate = 41.2072m;
+        public const decimal EuroRate = 45.0477m;
+
+        public decimal ConvertToDollars(decimal amountInHryvnia)
+        {
+            return amountInHryvnia / DollarRate;
+        }
+
+        public decimal ConvertToEuros(decimal amountInHryvnia)
+        {
+            return amountInHryvnia / EuroRate;
+        }
+
+        public decimal ConvertFromDollars(decimal amountInDollars)
+        {
+            return amountInDollars * DollarRate;
+        }
+
+        public decimal ConvertFromEuros(decimal amountInEuros)
+        {
+            return amountInEuros * EuroRate;
+        }
+    }
+}
---
+++ MyConverter/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace MyConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.WriteLine("Оберіть напрямок конвертації:");
+            Console.WriteLine("   1 - з гривні в долари");
+            Console.WriteLine("   2 - з гривні в євро");
+            Console.WriteLine("   3 - з доларів в гривні");
+            Console.WriteLine("   4 - з євро в гривні");
+
+            Converter сonverter = new Converter();
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Введіть суму в гривнях:");
+                    decimal amountHryvniaToDollars = Convert.ToDecimal(Console.ReadLine());
+                    Console.WriteLine($"Сума в доларах: {сonverter.ConvertToDollars(amountHryvniaToDollars)}");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Введіть суму в гривнях:");
+                    decimal amountHryvniaToEuros = Convert.ToDecimal(Console.ReadLine());
+                    Console.WriteLine($"Сума в євро: {сonverter.ConvertToEuros(amountHryvniaToEuros)}");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Введіть суму в доларах:");
+                    decimal amountDollars = Convert.ToDecimal(Console.ReadLine());
+                    Console.WriteLine($"Сума в гривнях: {сonverter.ConvertFromDollars(amountDollars)}");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Введіть суму в євро:");
+                    decimal amountEuros = Convert.ToDecimal(Console.ReadLine());
+                    Console.WriteLine($"Сума в гривнях: {сonverter.ConvertFromEuros(amountEuros)}");
+                    break;
+
+                default:
+                    Console.WriteLine("Невірний вибір.");
+                    break;
+            }
+        }
+    }
+}
